0f22bf8d0c073e12c5dbae09c68dd72f6b9894d6
shahid704 <68416468+shahid704@users.noreply.github.com>
2023-08-22 22:02:02 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,15 +15,15 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Shahid</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop User interfaces 
-            and web applications
-          </p>
-        </div>
+        <h1 className={`${styles.heroHeadText} text-white`}>
+          Hi, I'm <span className='text-[#915EFF]'>Shahid</span>
+        </h1>
+        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          I develop User interfaces 
+          and web applications
+        </p>
       </div>
+    </div>
 
       <ComputersCanvas />
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,15 @@
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useLoader, Canvas } from "@react-three/fiber";
-import { Suspense, useEffect, useState } from "react";
-import { OrbitControls, Preload } from "@react-three/drei";
-import Loader from "../Loader";
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = ({ isMobile = false }) => {
-  const gltf = useLoader(GLTFLoader, "/desktop_pc/scene.gltf");
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
     <mesh>
-      <hemisphereLight intensity={3.5} groundColor="black" />
-      <pointLight intensity={1} />
+      <hemisphereLight intensity={3.5} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,11 +18,12 @@ const Computers = ({ isMobile = false }) => {
         castShadow
         shadow-mapSize={1024}
       />
+      <pointLight intensity={1} />
       <primitive
+        object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
-        object={gltf.scene}
       />
     </mesh>
   );
@@ -31,51 +32,51 @@ const Computers = ({ isMobile = false }) => {
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
-  // useEffect(() => {
-  //   const mediaQuery = window.matchMedia("(max-width: 500px)");
+  useEffect(() => {
+      const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-  //   setIsMobile(mediaQuery.matches);
+      setIsMobile(mediaQuery.matches);
 
-  //   const onMediaQueryChange = (event: MediaQueryListEvent) => {
-  //     setIsMobile(event.matches);
-  //   };
+      const onMediaQueryChange = (event) => {
+          setIsMobile(event.matches);
+      };
 
-  //   mediaQuery.addEventListener("change", onMediaQueryChange);
+      mediaQuery.addEventListener("change", onMediaQueryChange);
 
-  //   return () => {
-  //     mediaQuery.removeEventListener("change", onMediaQueryChange);
-  //   };
-  // }, []);
+      return () => {
+          mediaQuery.removeEventListener("change", onMediaQueryChange);
+      };
+  }, []);
 
   const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    if (!initialized) {
-      setInitialized(true);
-    }
+      if (!initialized) {
+          setInitialized(true);
+      }
   }, [initialized]);
 
   if (!initialized) {
-    return <div></div>;
+      return <div></div>;
   }
 
   return (
-    <Canvas
-      frameloop="demand"
-      shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true, alpha: true }}
-    >
-      <Suspense fallback={<Loader />}>
-        {/* <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        /> */}
-        <Computers isMobile={isMobile} />
-      </Suspense>
-      <Preload all />
-    </Canvas>
+      <Canvas
+          frameloop="demand"
+          shadows
+          camera={{ position: [20, 3, 5], fov: 25 }}
+          gl={{ preserveDrawingBuffer: true, alpha: true }}
+      >
+          <Suspense fallback={<CanvasLoader/>}>
+              <OrbitControls
+                  enableZoom={false}
+                  maxPolarAngle={Math.PI / 2}
+                  minPolarAngle={Math.PI / 2}
+              />
+              <Computers isMobile={isMobile}/>
+          </Suspense>
+          <Preload all/>
+      </Canvas>
   );
 };
 
