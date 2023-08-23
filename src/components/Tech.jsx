2723a429ad67071c5b75c3d7d39184332c3ccbb9
shahid704 <68416468+shahid704@users.noreply.github.com>
2023-08-23 23:44:25 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [numBalls, setNumBalls] = useState(10); // Default number of balls
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 600) {
+        setNumBalls(5);
+      } else {
+        setNumBalls(10);
+      }
+    };
+
+    handleResize(); // Call it initially
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
+      {technologies.slice(0, numBalls).map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
